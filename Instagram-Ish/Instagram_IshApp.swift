bb1023dc0cb005c9e34cd6549e2d60bd3ffdb8d0
Derrick White <gr8white.contact@gmail.com>
2023-07-17 20:52:02 -0500

    Initial Commit

---
+++ Instagram-Ish/Instagram_IshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_IshApp.swift
+//  Instagram-Ish
+//
+//  Created by Cool-Ish on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_IshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
