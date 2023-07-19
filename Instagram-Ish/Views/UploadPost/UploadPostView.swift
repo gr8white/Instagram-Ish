7ab35764bb5208204f4d317dafafcff1f7aadba3
Derrick White <gr8white.contact@gmail.com>
2023-07-19 18:22:30 -0500

    Build Upload Post View

--- Instagram-Ish/Views/UploadPost/UploadPostView.swift
+++ Instagram-Ish/Views/UploadPost/UploadPostView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText: String = ""
+    
     var body: some View {
-        Text("Upload Posts")
+        VStack {
+            if postImage != nil {
+                Button {} label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                }
+            } else {
+                HStack(alignment: .top) {
+                    Image("batman")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption...", text: $captionText)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }
+                .padding()
+            }
+            
+            Spacer()
+        }
     }
 }
 
