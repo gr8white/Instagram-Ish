cea60f6fdee6a20fe6a43d66de5d12d4bdf5861f
Derrick White <gr8white.contact@gmail.com>
2023-07-19 19:29:35 -0500

    Derrick/feat/upload post view (#5)
    * Build Upload Post View
    
    * Selecting Post Image

--- Instagram-Ish/Views/UploadPost/UploadPostView.swift
+++ Instagram-Ish/Views/UploadPost/UploadPostView.swift
@@ -8,8 +8,64 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText: String = ""
+    @State var imagePickerPresented: Bool = false
+    
     var body: some View {
-        Text("Upload Posts")
+        VStack {
+            if postImage == nil {
+                Button {
+                    imagePickerPresented = true
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                }
+                .sheet(isPresented: $imagePickerPresented, onDismiss: loadImage) {
+                    ImagePicker(image: $selectedImage)
+                }
+            } else if let image = postImage {
+                HStack(alignment: .top) {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption...", text: $captionText)
+                }
+                .padding()
+                
+                Button {
+                    
+                } label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }
+                .padding()
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+extension UploadPostView {
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        
+        postImage = Image(uiImage: selectedImage)
     }
 }
 
