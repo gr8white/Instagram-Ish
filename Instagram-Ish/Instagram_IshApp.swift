6628441a4c5f95897701f2601be4d8747b2c2151
Derrick White <gr8white.contact@gmail.com>
2023-07-20 12:33:19 -0500

    Init SignInView

--- Instagram-Ish/Instagram_IshApp.swift
+++ Instagram-Ish/Instagram_IshApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Instagram_IshApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LandingView()
         }
     }
 }
